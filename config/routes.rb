38c0e0f676d67f221aaf1195b9d65c174ac20c29
sushiguruma <sushiguruma.honeycomb321@gmail.com>
2018-11-29 12:40:50 +0000

    rakuten api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'items/new'
+
   root to: 'toppages#index'
   
   get 'login', to: 'sessions#new'
@@ -7,4 +9,6 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
+  
+  resources :items, only: [:new]
 end
