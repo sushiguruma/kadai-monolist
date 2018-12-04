280fcddafb191cfd8e78e8e954d4b0abb32d523f
sushiguruma <sushiguruma.honeycomb321@gmail.com>
2018-12-04 11:26:15 +0000

    have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -16,6 +16,12 @@ class OwnershipsController < ApplicationController
       flash[:success] = '商品をWantしました。'
     end
     
+    #Have関係として保存
+    if params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品をHaveしました。'
+    end
+    
     redirect_back(fallback_location: root_path)
   end
 
@@ -25,6 +31,11 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.unwant(@item)
       flash[:success] = '商品のWantを削除しました。'
+    end
+    
+    if params[:type] == 'Have'
+      current_user.unhave(@item)
+      flash[:success] == '商品のHaveを削除しました。'
     end
     
     redirect_back(fallback_location: root_path)
